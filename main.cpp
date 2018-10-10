4b698535d96282f272ea675334f21b57305533da
alexandergol <43982046+alexandergol@users.noreply.github.com>
2018-10-10 10:22:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,26 +5,26 @@ using namespace std;
 int fizzbuzz(int i)
 {   
         if (i % 3 == 0){
-            cout<<"Fizz";
+         cout<<"Fizz";
         }
 
         if (i % 5 == 0){
-           cout<<"Bizz";
+        cout<<"Bizz";
         }
 
         if (i % 5 != 0 && i % 3 != 0)
     
-	  return -1;
+       return -1;
 }
 
 int main() 
 {   
-		fizzbuzz(9);
-		cout<<"\n";
-		fizzbuzz(10);
-		cout<<"\n";
-		fizzbuzz(15);
-		cout<<"\n";
+	fizzbuzz(9);
+	cout<<"\n";
+	fizzbuzz(10);
+	cout<<"\n";
+	fizzbuzz(15);
+	cout<<"\n";
       return 0;
 }
 
