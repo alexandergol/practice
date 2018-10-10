cd922480f8098cc4106574abd99ea247d490d435
alexandergol <43982046+alexandergol@users.noreply.github.com>
2018-10-10 10:20:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+
 using namespace std;
-int fizzbuzz(int i, const int n)
-{
-    for (i; i <= n; i++) {
-        if (i % 3 == 0) {
+
+int fizzbuzz(int i)
+{   
+        if (i % 3 == 0){
             cout<<"Fizz";
         }
-        if (i % 5 == 0) {
-          cout<<"Bizz";
+
+        if (i % 5 == 0){
+           cout<<"Bizz";
         }
+
         if (i % 5 != 0 && i % 3 != 0)
-            cout<<i;
-        cout<<"\n";
-    }
+    
 	  return -1;
 }
 
-	int main() 
-	{
-		fizzbuzz(1,100);
+int main() 
+{   
+		fizzbuzz(9);
 		cout<<"\n";
-		fizzbuzz(1,200);
+		fizzbuzz(10);
+		cout<<"\n";
+		fizzbuzz(15);
+		cout<<"\n";
+      return 0;
+}
 
-	}
 
