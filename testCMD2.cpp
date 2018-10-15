755289ad81418899248044d30ff3dabbb97d67d7
alexandergol <43982046+alexandergol@users.noreply.github.com>
2018-10-15 11:20:12 +0300

    Add files via upload

---
+++ testCMD2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include <string>
+
+
+using namespace std;
+   
+
+    int fizzbuzz(int i)
+{   
+        if (i % 3 == 0){
+            cout<<"Fizz";
+        }
+
+        if (i % 5 == 0){
+           cout<<"Buzz";
+        }
+
+        if (i % 5 != 0 && i % 3 != 0)
+    
+	  return 0;
+		}
+
+	string st1;
+
+	int main( int argc, char** argv) 
+
+{       
+	    for (int i = 0; i < argc; i++) {
+	      st1=argv[i];}
+
+	    if(st1!="fizzbuzz")
+          cout<<"Nothing to do"<<endl;
+
+		
+
+	    else if (st1=="fizzbuzz"){
+
+		fizzbuzz(9);
+		cout<<"\n";
+		fizzbuzz(10);
+		cout<<"\n";
+		fizzbuzz(15);
+		cout<<"\n";
+	}
+	
+      return 0;
+}
+
