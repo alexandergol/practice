3914ed5c0507edf0944bf4557b077a5a370930b0
Alexandr Golubev <agolubev1195@gmail.com>
2018-10-23 12:11:40 +0300

    Update testCMD.cpp

--- testCMD.cpp
+++ testCMD.cpp
@@ -1,13 +1,13 @@
-#include <iostream> 
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char** argv) { 
+  int main(int argc, char** argv) {
 
-  cout << "Have " << argc << " arguments:" <<endl; 
+    cout << "Have " << argc << " arguments:" <<endl;
 
-  for (int i = 0; i < argc; i++) {
-  cout << argv[i] << endl;
-  }
+        for (int i = 0; i < argc; i++) {
+        cout << argv[i] << endl;
+        }
   return 0;
 }
